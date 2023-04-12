2402e63c85aeaf3b5e6b8fcacef38f605f69b586
Seemanshu Shukla <seemanshu.shukla11@gmail.com>
2023-04-13 02:36:00 +0530

    Data Ingestion followed by Data tranformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-#from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformation
 
 
 ## Intitialize the Data Ingetion Configuration
@@ -48,8 +48,13 @@ class DataIngestion:
             logging.info('Exception occured at Data Ingestion stage')
             raise CustomException(e,sys)
 
+
+'''
 ## Run Data Ingestion
 
 if __name__=='__main__':
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+'''
